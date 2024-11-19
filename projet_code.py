cc1bdb9cde6d2fcc9bf27e3ded0aa49fc175b60f
Kévin BERNARD <kevin.bernard1901@gmail.com>
2024-11-19 16:42:51 +0100

    Modif projet_code Coup de pied Damien et Clarisse

--- projet_code.py
+++ projet_code.py
@@ -1,5 +1,3 @@
-# Projet test profondeur
-
 from pykinect2 import PyKinectV2
 from pykinect2 import PyKinectRuntime
 import cv2
@@ -25,8 +23,11 @@ def draw_body(screen, joints):
         if z > 0:
             x, y = int(x * 100 + width // 2), int(-y * 100 + height // 2)
             pygame.draw.circle(screen, (255, 0, 0), (x, y), 5)
-
-compteur = 0
+            
+# Fonction d'affichage test
+def fonction_affichage():
+    for i in range(1000):
+        print(i)
 
 # Boucle principale
 running = True
@@ -46,22 +47,21 @@ while running:
                 joints = body.joints
                 
                 # Indice des jointures à tester
-                indice_main_droite = 11
-                indice_coude_droit = 9
+                indice_pied_droit = 19
+                indice_genoux_gauche = 13
                 
                 # Récupération des y
-                x_main_droite = joints[indice_main_droite].Position.x
-                x_coude_droit = joints[indice_coude_droit].Position.x
+                y_pied_droit = joints[indice_pied_droit].Position.y
+                y_genoux_gauche = joints[indice_genoux_gauche].Position.y
                 
-                # Affichage des distances
-                print("Numero ", compteur, "distance : ",abs(x_main_droite - x_coude_droit))
-                compteur+=1
-                
-                if compteur > 20 :
-                    running=False
-                
-                # Faire attendre pour éviter de prendre plusieurs fois la même acquisition
-                time.sleep(1)
+                # Test de comparaison
+                if y_pied_droit > y_genoux_gauche : 
+                    print("Pied droit plus haut que genou gauche")
+                    # KEVIN !!!!! A REMPLIIIIIIR !!!! Lancer l'animation
+                    fonction_affichage()
+                    print("####################################################")
+                    running = False
+                    
 
         if kinect.has_new_color_frame():
             frame = kinect.get_last_color_frame()
@@ -80,92 +80,6 @@ while running:
 # Nettoyer
 pygame.quit()
 kinect.close()
-
-
-############################################################################"
-# Projet code cop de pied
-
-# from pykinect2 import PyKinectV2
-# from pykinect2 import PyKinectRuntime
-# import cv2
-# import pygame
-# import openpyxl
-# import time
-
-# # Initialisation de la kinect
-# kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body | PyKinectV2.FrameSourceTypes_Color)
-
-# # Initialisation de pygame
-# pygame.init()
-# width, height = 960, 540
-# screen = pygame.display.set_mode((width, height))
-
-# # Fonction pour dessiner un squelette
-# def draw_body(screen, joints):
-#     for joint in joints:
-#         print(type(joints))
-#         print("je suis là")
-#         position = joints[joint].Position
-#         x, y, z = position.x, position.y, position.z
-#         if z > 0:
-#             x, y = int(x * 100 + width // 2), int(-y * 100 + height // 2)
-#             pygame.draw.circle(screen, (255, 0, 0), (x, y), 5)
-
-# compteur = 0
-
-# # Boucle principale
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-    
-#     # Récupération des données de la kinect
-#     if kinect.has_new_body_frame():
-#         bodies = kinect.get_last_body_frame()
-#         if bodies != None:
-#             for i in range(0, kinect.max_body_count):
-#                 body = bodies.bodies[i]
-#                 if not body.is_tracked:
-#                     continue
-#                 joints = body.joints
-                
-#                 # Indice des jointures à tester
-#                 indice_pied_droit = 19
-#                 indice_genoux_gauche = 13
-                
-#                 # Récupération des y
-#                 y_pied_droit = joints[indice_pied_droit].Position.y
-#                 y_genoux_gauche = joints[indice_genoux_gauche].Position.y
-                
-#                 # Test de comparaison
-#                 if y_pied_droit > y_genoux_gauche : 
-#                     print("Essai N°",compteur," Le pied droit est plus haut que le genou gauche")
-#                     compteur += 1
-                
-#                 if compteur > 15 :
-#                     running=False
-                
-#                 # Faire attendre pour éviter de prendre plusieurs fois la même acquisition
-#                 time.sleep(0.5)
-
-#         if kinect.has_new_color_frame():
-#             frame = kinect.get_last_color_frame()
-            
-#             if frame is not None:
-#                 # Redimensionnement et transformation de l'image
-#                 frame = frame.reshape((1080, 1920, 4))  # Reshape du frame pour 1080p
-#                 frame = frame[:, :, :3]  # Supprimer le canal alpha
-#                 frame = cv2.resize(frame, (width, height))  # Redimensionner avec OpenCV
-#                 frame = pygame.surfarray.make_surface(frame.swapaxes(0, 1))  # Créer une surface Pygame
-                
-#                 # Afficher le frame redimensionné
-#                 screen.blit(frame, (0, 0))
-#                 pygame.display.flip()  # Mettre à jour l'affichage
-
-# # Nettoyer
-# pygame.quit()
-# kinect.close()
 
 
 # # Test
